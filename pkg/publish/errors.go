ed0faf23262222f873598710110501240619c97a
Leonardo Miranda Amaral <leonardo.miranda@suse.com>
2026-05-08 16:25:19 -0300

    add ErrInvalidVersion sentinel to publish package

--- pkg/publish/errors.go
+++ pkg/publish/errors.go
@@ -6,6 +6,7 @@ var (
 	ErrNotImplemented              = errors.New("publish.Workflow: method not implemented")
 	ErrInvalidTransition           = errors.New("invalid phase transition")
 	ErrBundleNotFound              = errors.New("bundle not found")
+	ErrInvalidVersion              = errors.New("invalid proposed version format")
 	ErrPublishConflict             = errors.New("blueprint version already exists")
 	ErrPublishNotPending           = errors.New("bundle is not in submitted state")
 	ErrPublishVersionNotIncreasing = errors.New("proposed version must be greater than existing")
