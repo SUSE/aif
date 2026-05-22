9b3eba8f817b541d566ed5260a7d24830f7c55bf
Thiago Bertoldi <thiago.bertoldi@suse.com>
2026-05-22 15:30:14 -0300

    P4-3b: add verify-mock examples (CR build + phase mapping)

---
+++ pkg/fleet/export_test.go
@@ -0,0 +1,5 @@
+package fleet
+
+// BuildBundleCRForTest re-exports buildBundleCR for black-box example tests.
+// Internal callers should use buildBundleCR directly.
+var BuildBundleCRForTest = buildBundleCR
